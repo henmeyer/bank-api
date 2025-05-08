b403cbafb4974e891f812bfbd9c44d632b22c041
henmeyer <henriquemmeyer@gmail.com>
2025-05-07 23:11:25 -0300

    Refactor: remove switch case from EventService

--- src/services/event/EventService.ts
+++ src/services/event/EventService.ts
@@ -5,16 +5,19 @@ import accountService from "../account/AccountService";
 const handleEvent = (payload: IEvent): IAccount | undefined => {
   const { type, origin, destination, amount } = payload;
 
-  switch (type) {
-    case "deposit":
-      return accountService.deposit(destination, amount);
-    case "withdraw":
-      return accountService.withdraw(origin, amount);
-    case "transfer":
-      return accountService.transfer(origin, destination, amount);
-    default:
-      throw new Error("Invalid event type");
+  const handlers = {
+    deposit: () => accountService.deposit(destination, amount),
+    withdraw: () => accountService.withdraw(origin, amount),
+    transfer: () => accountService.transfer(origin, destination, amount),
+  };
+
+  const handler = handlers[type];
+
+  if (!handler) {
+    throw new Error("Invalid event type");
   }
+
+  return handler();
 };
 
 export default { handleEvent };
